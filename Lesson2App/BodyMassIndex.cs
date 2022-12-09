d252013ce18712bf6dea7941f92470cdf5037257
l.kvachadze <l.kvachadze@SB604>
2022-12-09 14:40:05 +0400

    Add project files.

---
+++ Lesson2App/BodyMassIndex.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2App
+{
+    public static class BodyMassIndex
+    {
+        static float height;
+        static float weight;
+
+        public static void BmiCalculate()
+        {
+            Console.WriteLine("What is your name?");
+            string myName = Console.ReadLine();
+
+            Console.WriteLine("What is your lastname?");
+            string mylastName = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Please Write What is your Height in Metric?");
+            float myHeight = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Please Write What is your Weight in kilogram?");
+            float myWeight = Convert.ToSingle(Console.ReadLine());
+
+            
+            float bmi = myWeight/(myHeight * myHeight);
+         
+
+            height = myHeight;
+            weight = myWeight;
+
+            Console.WriteLine("your name is : "  + myName + " " + mylastName );
+            Console.WriteLine($"Your age is {age}");
+            Console.WriteLine($"Your Body Mass index is : {Math.Round(bmi)} \n");
+
+            if (bmi <25)
+            {
+                Console.WriteLine("You have Normal weight");
+            }
+            else if(bmi >=25 && bmi <= 30)
+            {
+                Console.WriteLine("You have overweight");
+            }
+            else if (bmi > 30 && bmi <= 40)
+            {
+                Console.WriteLine("You have Obesity");
+            }
+            else if (bmi > 40)
+            {
+                Console.WriteLine("You have Morbid Obesity");
+            }
+            else
+            {
+                Console.WriteLine("something went wrong");  
+            }
+        }
+    }
+}
---
+++ Lesson2App/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2App
+{
+  
+    internal class Program
+    {
+             public static int MyArray()
+             {
+                int[] arrays = new int[10] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+
+                int arraySum = arrays[0] + arrays[9];
+                 return arraySum;
+
+             }
+
+          public static void PlusNumbers()
+        {
+            int x = 10;
+            int y = 15;
+            int sum = x + y;
+            Console.WriteLine($"ori ricxvis jam aris : {sum}");
+        }
+
+        public static double AddDouble()
+        {
+             Console.WriteLine("Choose your first double");
+             double a = Convert.ToDouble(Console.ReadLine());
+
+             Console.WriteLine("Choose your second double");
+             double b = Convert.ToDouble(Console.ReadLine());
+             double c = a + b;
+            return c;
+        }
+
+        static void Main(string[] args)
+        {
+             PlusNumbers(); //სავარჯიშო 1
+            
+            Console.Beep();
+            Console.Title = "Lesson2App";
+            Console.WriteLine("-----------------------------------------------\n");
+
+            
+            Console.WriteLine( "Sum of arrays first and last index is " + Program.MyArray()); //სავარჯიშო 2
+            Console.WriteLine("----------------------------------------------- \n");
+
+            
+            Console.WriteLine($"Double Sum is : {AddDouble()}");  //სავარჯიშო 3
+            Console.WriteLine("-----------------------------------------------\n");
+
+
+            BodyMassIndex.BmiCalculate();//სავარჯიშო 4
+            Console.WriteLine("-----------------------------------------------\n");
+
+            Console.ReadKey();
+        }
+    }
+}
